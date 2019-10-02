1939d952c8ea592a95d0ef964df42c5ce9acda6b
mehsani <mehsani@users.noreply.github.com>
2019-10-02 13:21:23 +1000

    Add project files.

---
+++ WebDownloader/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDownloader.Models;
+
+namespace WebDownloader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartDownload(int irid, string url)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (irid == 9662)
+                    {
+                        Uri uri = new Uri(url);
+                        string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = $"/C \"C:\\Program Files (x86)\\Internet Download Manager>idman /d {url} /p C:\\inetpub\\wwwroot\\GalleryServer\\gs\\mediaobjects /f d.mkv /s /q /h /n\"";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ WebDownloader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDownloader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
